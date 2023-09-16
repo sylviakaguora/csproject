b34e08da2c0403532cae6926efa07e27e46bdc42
Sylvia Kaguora <sylivia.kaguora@strathmore.edu>
2023-09-16 14:53:29 +0300

    changes to Doctor class

--- drugstore/store/models.py
+++ drugstore/store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    specialty = models.OneToOneField(Specialty)
+    years_of_experience = models.IntegerField()
+    patients = models.ManyToOneRel(Patient) 
+
+
+
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=20)
